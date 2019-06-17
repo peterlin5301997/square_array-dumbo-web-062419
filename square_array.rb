b7f4ba2a3b9c24965fc33d829a3ce710c8e041d6
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 21:26:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   # your code here
+  new_numbers = []
   array.each do |number|
-    new_numbers = number**2
+    new_numbers.push(number**2)
   end
   return new_numbers
 end
